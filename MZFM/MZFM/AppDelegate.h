858774ec1560f262e71ec81c364350b700c49a60
naly <261621205@qq.com>
2017-06-08 21:05:58 +0800

    项目创建

---
+++ MZFM/MZFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MZFM
+//
+//  Created by 朱明钧 on 2017/6/8.
+//  Copyright © 2017年 朱明钧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MZFM/MZFMTests/MZFMTests.m
@@ -0,0 +1,39 @@
+//
+//  MZFMTests.m
+//  MZFMTests
+//
+//  Created by 朱明钧 on 2017/6/8.
+//  Copyright © 2017年 朱明钧. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MZFMTests : XCTestCase
+
+@end
+
+@implementation MZFMTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
